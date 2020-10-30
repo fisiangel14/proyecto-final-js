c71683a64de0be54b42b06c4e769d057aad291ab
Angel Alegre <fisiangel.14@gmail.com>
2020-10-30 16:38:18 -0500

    mostrar productos

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("HOLA")
+const productos = [
+    {
+        nombre: "Hamburguesa simpe",
+        costo: 20,
+        codigo: "H100"
+    }, 
+    {
+        nombre: "Hamburguesa doble",
+        costo: 30,
+        codigo: "H200"
+    },
+    {
+        nombre: "Papas fritas",
+        costo: 8,
+        codigo: "P100"
+    },
+    {
+        nombre: "Refresco personal",
+        costo: 6,
+        codigo: "R100"
+    }
+]
+
+const monstrarMenu = () => {
+    console.log("CODIGO - NOMBRE DEL PRODUCTO - COSTO")
+    productos.forEach(producto => console.log(`${producto.codigo} - ${producto.nombre} -- $${producto.costo} `))
+}
