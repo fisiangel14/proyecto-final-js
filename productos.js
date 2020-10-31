a45c2c4cd6eaa29933c7c48b6ccae5698c1cbb85
Angel Alegre <fisiangel.14@gmail.com>
2020-10-30 20:44:15 -0500

    multiples files js y orden de los mismos

---
+++ productos.js
@@ -0,0 +1,22 @@
+const productos = [
+    {
+        nombre: "Hamburguesa simpe",
+        costo: 20,
+        codigo: "H100"
+    }, 
+    {
+        nombre: "Hamburguesa doble",
+        costo: 30,
+        codigo: "H200"
+    },
+    {
+        nombre: "Papas fritas",
+        costo: 8,
+        codigo: "P100"
+    },
+    {
+        nombre: "Refresco personal",
+        costo: 6,
+        codigo: "R100"
+    }
+]
--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-const productos = [
-    {
-        nombre: "Hamburguesa simpe",
-        costo: 20,
-        codigo: "H100"
-    }, 
-    {
-        nombre: "Hamburguesa doble",
-        costo: 30,
-        codigo: "H200"
-    },
-    {
-        nombre: "Papas fritas",
-        costo: 8,
-        codigo: "P100"
-    },
-    {
-        nombre: "Refresco personal",
-        costo: 6,
-        codigo: "R100"
-    }
-]
+console.log(productos)
 
 const monstrarMenu = () => {
     console.log("CODIGO - NOMBRE DEL PRODUCTO - COSTO")
