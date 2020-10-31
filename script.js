fed463c665970e3871226c2d05d3aacd72194a72
Angel Alegre <fisiangel.14@gmail.com>
2020-10-30 20:58:22 -0500

    pedir producto

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-console.log(productos)
+const usuario = {
+    nombre: "Angel",
+    edad: 26,
+    deuda: 0
+}
+
+const pedido = []
 
 const monstrarMenu = () => {
     console.log("CODIGO - NOMBRE DEL PRODUCTO - COSTO")
     productos.forEach(producto => console.log(`${producto.codigo} - ${producto.nombre} -- $${producto.costo} `))
+}
+
+const pedirProducto = cod => {
+    if(!cod) return "Ingrese un codigo valido!"
+
+    const productoEncontrado = productos.find(producto => producto.codigo === cod)
+    
+    if(!productoEncontrado) return "El producto no existe!!!"
+
+    pedido.push(productoEncontrado)
+    console.log("Su producto fue agregado correctamente")
+    return pedido
 }
